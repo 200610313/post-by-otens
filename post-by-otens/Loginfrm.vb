7543f0930b7df26df6419d2a81d318a21cb065d9
Wanseeeeh <lanceparantar11@gmail.com>
2020-02-04 22:26:06 +0800

    Remove LoginFrm

--- post-by-otens/Loginfrm.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class Loginfrm
-    Private Sub AlphaBlendTextBox1_TextChanged(sender As Object, e As EventArgs) Handles AlphaBlendTextBox2.TextChanged, AlphaBlendTextBox1.TextChanged
-
-    End Sub
-
-    Private Sub AlphaBlendTextBox1_TextChanged_1(sender As Object, e As EventArgs)
-
-    End Sub
-End Class
