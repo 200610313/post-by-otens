ceb0d6eaf5eef57f6f77a88a2597d01daf051122
Angelu Garcia <200610313@my.xu.edu.ph>
2020-02-05 11:08:44 +0800

    Added register module

---
+++ post-by-otens/RegisterModule.vb
@@ -0,0 +1,15 @@
+﻿Module RegisterModule
+    Public Sub loadProductsOf(loggedInBusinessName As String, ByRef products As List(Of product))
+        Dim adapter As New POSDataSetTableAdapters.product1TableAdapter
+        Dim i, rows As Integer
+        rows = CInt(adapter.getProductsOf(loggedInBusinessName).Rows.Count)
+        For i = 0 To rows - 1
+            Dim prodName = adapter.getProductsOf(loggedInBusinessName).Rows(i).Item(0)
+            Dim prodPrice = adapter.getProductsOf(loggedInBusinessName).Rows(i).Item(1)
+            Dim prodStock = adapter.getProductsOf(loggedInBusinessName).Rows(i).Item(2)
+            Dim prodNumber = adapter.getProductsOf(loggedInBusinessName).Rows(i).Item(3)
+            products.Add(New product(prodName, prodNumber, prodPrice, prodStock))
+        Next
+    End Sub
+
+End Module
